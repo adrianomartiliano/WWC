173016489a31752ce0d29202bbdae405015a11e7
Adriano Martiliano <adrianomartiliano@gmail.com>
2022-07-12 12:44:41 -0300

    Estilização e forma
    Criando a estrutura do site.

--- index.php
+++ index.php
@@ -4,19 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>WWC</title>
-    <style>
-        body{background-color: grey;}
-        h1, h2, h5{color: white; text-align: center;}
-    </style>
 </head>
 <body>
-    <script>
-        alert("Em breve novidades!!");
-        alert("Fique ligado!!!");
-    </script>
-    <h1>World War 2 CUP</h1>
-    <h2>Em construção!!!!</h2>
-    <h5>By: G O K U & DALMO-ROJO</h5>
+    <header>
+        <div class="center">
+            <div class="logo left"><a href="index.php"><img src="images/wwc2.png" alt=""></a></div><!--logo-->
+            <nav class="desktop right">
+                <ul>
+                    <a href=""><li>REGRAS</li></a>
+                </ul>
+            </nav>
+            <nav class="mobile right">
+                <ul>
+                    <a href=""><li>REGRAS</li></a>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </div>
+    </header>
+    <div class="conteudo">
+        <h1>World War 2 CUP</h1>
+        <h2>Em construção!!!!</h2>
+    </div>
+    <footer><h5>By: G O K U & DALMO-ROJO</h5></footer>
 </body>
 </html>
