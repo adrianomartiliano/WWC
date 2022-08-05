9a63bc9c89c7554c2f5e22530e10610ce8b9bd83
Adriano Martiliano <adrianomartiliano@gmail.com>
2022-08-05 09:55:05 -0300

    Inserção de conteúdo.

--- card-capitao.php
+++ card-capitao.php
@@ -19,10 +19,9 @@
     <div class="conteudo">
 
 
-    
     <div class="card">
-        <div class="nome-equipe"><h1>CANNABIS</h1><h4>CANGACEIROS</h4></div>
-        <div class="nome-capitao"><h3><a href="#">Leleco-Mau-BR<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="nome-equipe"><h1>LOS GEDES</h1><h4>THREAT SIGNAL</h4></div>
+        <div class="nome-capitao"><h3><a href="#">Agus el father<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
         <div class="horario">
             <div class="hor-atual">
                 <?php
@@ -34,26 +33,10 @@
             </div>
             <img src="icons/brazil_flags_flag_9043.ico"></img>
         </div>
-    </div>
+    </div><!--Los gedes-->
     <div class="card">
-        <div class="nome-equipe"><h1>JAGUNÇOS</h1><h4>CANGACEIROS</h4></div>
-        <div class="nome-capitao"><h3><a href="#">CORISCO<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
-        <div class="horario">
-            <div class="hor-atual">
-                <?php
-                    $Object = new DateTime();  
-                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
-                    $DateAndTime = $Object->format("h:i:s a");
-                    echo "Horário Atual";
-                ?>
-        </div>
-            <img src="icons/brazil_flags_flag_9043.ico"></img>
-    </div>
-    </div>
-
-    <div class="card">
-        <div class="nome-equipe"><h1>CLAN-DESTINOS</h1><h4>CANGACEIROS</h4></div>
-        <div class="nome-capitao"><h3><a href="#">KARKARA<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="nome-equipe"><h1>DEPREDADORES</h1><h4>MUNDIALES - MUNU</h4></div>
+        <div class="nome-capitao"><h3><a href="#">CAPTANIJONNI<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
         <div class="horario">
             <div class="hor-atual">
                 <?php
@@ -65,9 +48,9 @@
             </div>
             <img src="icons/brazil_flags_flag_9043.ico"></img>
         </div>
-    </div>
+    </div><!--DEPREDADORES-->
     <div class="card">
-        <div class="nome-equipe"><h1 style="font-size:20px">VETERANOS DE GUERRA</h1><h4>MUNDIALES</h4></div>
+        <div class="nome-equipe"><h1 style="font-size:20px">VETERANOS DE GUERRA</h1><h4>MUNDIALES - MUND</h4></div>
         <div class="nome-capitao"><h3><a href="#">DALMO-ROJO<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
         <div class="horario">
             <div class="hor-atual">
@@ -80,8 +63,146 @@
             </div>
             <img src="icons/argentina-bandeira.ico"></img>
         </div>
-    </div>
-    </div>
+    </div><!--VETERANOS DE GUERRA-->
+    <div class="card">
+        <div class="nome-equipe"><h1 style="font-size:20px">TERRORISTAS DELTA</h1><h4>TRRD</h4></div>
+        <div class="nome-capitao"><h3><a href="#">Das_trevas<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+            </div>
+            <img src="icons/argentina-bandeira.ico"></img>
+        </div>
+    </div><!--TERRORISTAS DELTA-->
+    <div class="card">
+        <div class="nome-equipe"><h1>BLACK ORDERS</h1><h4>GNR</h4></div>
+        <div class="nome-capitao"><h3><a href="#">DEVIL<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+            </div>
+            <img src="icons/brazil_flags_flag_9043.ico"></img>
+        </div>
+    </div><!--BLACK ORDERS-->
+    <div class="card">
+        <div class="nome-equipe"><h1 style="font-size:20px">LOS 4 FANTÁSTICOS</h1><h4>THREAT SIGNAL</h4></div>
+        <div class="nome-capitao"><h3><a href="#">dulcevenganza777<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+            </div>
+            <img src="icons/argentina-bandeira.ico"></img>
+        </div>
+    </div><!--LOS 4 FANTÁSTICOS-->
+    <div class="card">
+        <div class="nome-equipe"><h1>UNITED</h1><h4>MUNDIALES - MUNU</h4></div>
+        <div class="nome-capitao"><h3><a href="#">EyedEnd32<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+        </div>
+            <img src="icons/brazil_flags_flag_9043.ico"></img>
+        </div>
+    </div><!--UNITED-->
+
+    <div class="card">
+        <div class="nome-equipe"><h1>PANDILLEROS</h1><h4>GTRS</h4></div>
+        <div class="nome-capitao"><h3><a href="#">GTK-0709<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+            </div>
+            <img src="icons/brazil_flags_flag_9043.ico"></img>
+        </div>
+    </div><!--PANDILLEROS-->
+    <div class="card">
+        <div class="nome-equipe"><h1>GLORIOSOS PATRIOTAS</h1><h4>VENEZUELA</h4></div>
+        <div class="nome-capitao"><h3><a href="#">JЯ-     GЦЭRЯД<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+            </div>
+            <img src="icons/brazil_flags_flag_9043.ico"></img>
+        </div>
+    </div><!--GLORIOSOS PATRIOTAS-->
+    <div class="card">
+        <div class="nome-equipe"><h1>CLAN-DESTINOS</h1><h4>СДИGДСЭIЯOS</h4></div>
+        <div class="nome-capitao"><h3><a href="#">KARKARA<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+            </div>
+            <img src="icons/brazil_flags_flag_9043.ico"></img>
+        </div>
+    </div><!--CLAN-DESTINOS-->
+    <div class="card">
+        <div class="nome-equipe"><h1>Z SPECIAL UNIT</h1><h4>WINNERS</h4></div>
+        <div class="nome-capitao"><h3><a href="#">KING PITBULL<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+            </div>
+            <img src="icons/brazil_flags_flag_9043.ico"></img>
+        </div>
+    </div><!--Z SPECIAL UNIT-->
+    <div class="card">
+        <div class="nome-equipe"><h1>Z SPECIAL UNIT</h1><h4>WINNERS</h4></div>
+        <div class="nome-capitao"><h3><a href="#">KING PITBULL<span class="icon-whats"><i class="fa-brands fa-whatsapp"></i></span></h3></a></div>
+        <div class="horario">
+            <div class="hor-atual">
+                <?php
+                    $Object = new DateTime();  
+                    $Object->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+                    $DateAndTime = $Object->format("h:i:s a");
+                    echo "Horário Atual";
+                ?>
+            </div>
+            <img src="icons/brazil_flags_flag_9043.ico"></img>
+        </div>
+    </div><!--Z SPECIAL UNIT-->
+
+    
+    </div><!--CONTEUDO->
 
 
     </body>
--- index.php
+++ index.php
@@ -16,29 +16,29 @@
         <br/><br/>
         <h2>O maior torneio de WW2, feito de jogador para jogador!!</h2><br/><br/>
         <h2>Equipes Confirmadas</h2><br/>
-        <h4>CAN - CДNИДBIS</h4>
-        <h4>CAN - JДGЦИÇФS</h4>
-        <h4>MUNU - UNITED</h4>
-        <h4>MUND - VETERANOS DE GUERRA</h4>
-        <h4>MUND - KAMIKAZES</h4>
-        <h4>WU - PRO GAMING</h4>
-        <h4>SWBR - SWAT BRASIL PRO</h4>
-        <h4>GTRS - PANDILLEROS</h4>
-        <h4>TRRD - TERROSTIAS DELTA</h4>
-        <h4>VDLM - DEATH SQUAD</h4>
-        <h4>VZLA - HEROES DE LA PATRIA</h4>
-        <h4>VZLA - GLORIOSOS PATRIOTAS</h4>
-        <h4>TS - LOS VERDUGOS</h4>
-        <h4>TS - LOS GEDES</h4>
-        <h4>TS - HUNTERS</h4>
-        <h4>CAN - CLAN-DESTINOS</h4>
-        <h4>TEAM - BLACK ORDERS</h4>
-        <h4>TS - LOS 4 FANTÁSTICOS</h4>
-        <h4>WINN - Z SPECIAL UNIT</h4>
-        <h4>WINN - THE KILL TEAM</h4>
-        <h4>TEAM - SONS OF THOR</h4>
-        <h4>MUNM - BESTIAS DE ASALTO</h4>
-        <h4>CAN - CLAN-PEÕES</h4>
+        <h4>1 - CAN - CДNИДBIS</h4>
+        <h4>2 - CAN - JДGЦИÇФS</h4>
+        <h4>3 - MUNU - UNITED</h4>
+        <h4>4 - MUND - VETERANOS DE GUERRA</h4>
+        <h4>5 - MUND - KAMIKAZES</h4>
+        <h4>6 - WU - PRO GAMING</h4>
+        <h4>7 - SWBR - SWAT BRASIL PRO</h4>
+        <h4>8 - GTRS - PANDILLEROS</h4>
+        <h4>9 - TRRD - TERROSTIAS DELTA</h4>
+        <h4>10 - VDLM - DEATH SQUAD</h4>
+        <h4>11 - VZLA - HEROES DE LA PATRIA</h4>
+        <h4>12 - VZLA - GLORIOSOS PATRIOTAS</h4>
+        <h4>13 - TS - LOS VERDUGOS</h4>
+        <h4>14 - TS - LOS GEDES</h4>
+        <h4>15 - TS - HUNTERS</h4>
+        <h4>16 - CAN - CLAN-DESTINOS</h4>
+        <h4>17 - GNR - BLACK ORDERS</h4>
+        <h4>18 - TS - LOS 4 FANTÁSTICOS</h4>
+        <h4>19 - WINN - Z SPECIAL UNIT</h4>
+        <h4>20 - WINN - THE KILL TEAM</h4>
+        <h4>21 - TEAM - SONS OF THOR</h4>
+        <h4>22 - MUNM - BESTIAS DE ASALTO</h4>
+        <h4>23 - CAN - CLAN-PEÕES</h4>
         
     </div>
     </div>
