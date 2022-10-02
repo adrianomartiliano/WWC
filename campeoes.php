c8fc77d115e01b7f141b34e9bfb6f0cab55886bc
Adriano Martiliano <adrianomartiliano@gmail.com>
2022-10-01 22:41:48 -0300

    Página de Campeão

---
+++ campeoes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/campeoes.css">
+    <title>CAMPEÕES</title>
+</head>
+<body>
+    <?php
+        require_once 'menu.html';
+    ?>
+
+    <section class="campeoes">
+        <div class="center">
+            <div class="conteudo-campeoes">
+                <div class="clans-campeoes"><h1>CLANS</h1></div>
+                <div class="jogadores-campeoes"><h1>JOGADORES</h1></div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
