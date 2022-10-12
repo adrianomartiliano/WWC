c1973340792b0a27e8f5ed63d89e0bf7eb91e7c0
Adriano Martiliano <adrianomartiliano@gmail.com>
2022-10-11 22:47:01 -0300

    Iniciando Estilização - Campeões

--- campeoes.php
+++ campeoes.php
@@ -15,8 +15,25 @@
     <section class="campeoes">
         <div class="center">
             <div class="conteudo-campeoes">
-                <div class="clans-campeoes"><h1>CLANS</h1></div>
-                <div class="jogadores-campeoes"><h1>JOGADORES</h1></div>
+                <div class="clans-campeoes">
+                    <div class="list-clan"><p>WU</p><img src="images/ourox4.png" alt=""></div>
+                    <div class="list-clan"><p>CANGACEIROS</p><img src="images/pratax4.png" alt=""></div>
+                    <div class="list-clan"><p>WINNERS</p><img src="images/bronzex4.png" alt=""></div>
+                </div>
+                <div class="jogadores-campeoes inativo">
+                    <div class="list-jogadores"><p>B O S S</p><img src="images/ourox4.png" alt=""><img src="images/ourox4.png" alt=""></div>
+                    <div class="list-jogadores"><p>1420lopez</p><img src="images/ourox4.png" alt=""></div>
+                    <div class="list-jogadores"><p>Joey BRICKWALL</p><img src="images/ourox4.png" alt=""></div>
+                    <div class="list-jogadores"><p>ILLUSTRIOUS ONER</p><img src="images/ourox4.png" alt=""></div>
+                    <div class="list-jogadores"><p>G Ф K Ц</p><img src="images/pratax4.png" alt=""></div>
+                    <div class="list-jogadores"><p>LeLecф_Ф_Mдu_BЯ</p><img src="images/pratax4.png" alt=""></div>
+                    <div class="list-jogadores"><p>Maltryx</p><img src="images/pratax4.png" alt=""></div>
+                    <div class="list-jogadores"><p>BLДCK -Ф- KELLДЯ</p><img src="images/pratax4.png" alt=""></div>
+                    <div class="list-jogadores"><p>GOD OF WAR</p><img src="images/bronzex4.png" alt=""></div>
+                    <div class="list-jogadores"><p>ANGEL MALVADO</p><img src="images/bronzex4.png" alt=""></div>
+                    <div class="list-jogadores"><p>TERMINATOR</p><img src="images/bronzex4.png" alt=""></div>
+                    <div class="list-jogadores"><p>KING PITBULL</p><img src="images/bronzex4.png" alt=""></div>
+                </div>
             </div>
         </div>
     </section>
