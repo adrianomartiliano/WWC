74b6ff79b97db948d5cdfeef0fee9d25b1a05b42
Adriano Martiliano <adrianomartiliano@gmail.com>
2022-12-21 14:42:10 -0300

    Atualização de dados

--- x2.php
+++ x2.php
@@ -25,43 +25,50 @@
                     <th colspan='3'>#</th><th colspan='3'>DUPLA</th><th colspan='3'>P</th><th colspan='3'>K</th><th colspan='3'>PC</th>
                 </tr>
                         <tr>
-                            <td colspan='1'>1º</td><th colspan='3' width='200px'>-KingPitbull & Terminator</th><th colspan='3'>3</th><th colspan='3'>40</th><th colspan='3'>631</th>
+                            <td colspan='1'>1º</td><th colspan='3' width='200px'>-KingPitbull & Terminator</th><th colspan='3'>6</th><th colspan='3'>65</th><th colspan='3'>1.044</th>
                         </tr>
                         <tr>
-                            <td colspan='1'>2º</td><th colspan='3' width='200px'>-Will_Killer77 & -BRZIKA-</th><th colspan='3'>3</th><th colspan='3'>18</th><th colspan='3'>302</th>
+                            <td colspan='1'>2º</td><th colspan='3' width='200px'>-Will_Killer77 & -BRZIKA-</th><th colspan='3'>6</th><th colspan='3'>28</th><th colspan='3'>502</th>
                         </tr>
                         <tr>
-                            <td colspan='1'>3º</td><th colspan='3' width='200px'>-DALMO ROJO- & ZEUS GOD SUPREME</th><th colspan='3'>3</th><th colspan='3'>16</th><th colspan='3'>300</th>
+                            <td colspan='1'>3º</td><th colspan='3' width='200px'>God Of War & H3dexqa</th><th colspan='3'>3</th><th colspan='3'>10</th><th colspan='3'>200</th>
                         </tr>
                         <tr>
-                            <td colspan='1'>4º</td><th colspan='3' width='200px'>-Un Peruanito- & El Palo Mudo</th><th colspan='3'>2</th><th colspan='3'>37</th><th colspan='3'>620</th>
+                            <td colspan='1'>4º</td><th colspan='3' width='200px'>-DALMO ROJO- & ZEUS GOD SUPREME</th><th colspan='3'>3</th><th colspan='3'>16</th><th colspan='3'>300</th>
                         </tr>
                         <tr>
-                            <td colspan='1'>5º</td><th colspan='3' width='200px'>UnFriendRedain & Darkest Starr</th><th colspan='3'>2</th><th colspan='3'>25</th><th colspan='3'>429</th>
+                            <td colspan='1'>5º</td><th colspan='3' width='200px'>SOTO & P90</th><th colspan='3'>3</th><th colspan='3'>10</th><th colspan='3'>200</th>
                         </tr>
                         <tr>
-                            <td colspan='1'>6º</td><th colspan='3' width='200px'>joleno matador & Das_Trevas</th><th colspan='3'>2</th><th colspan='3'>23</th><th colspan='3'>388</th>
+                            <td colspan='1'>6º</td><th colspan='3' width='200px'>-Un Peruanito- & El Palo Mudo</th><th colspan='3'>2</th><th colspan='3'>37</th><th colspan='3'>620</th>
                         </tr>
                         <tr>
-                            <td colspan='1'>7º</td><th colspan='3' width='200px'>BABAYAGA & MESSI</th><th colspan='3'>1</th><th colspan='3'>29</th><th colspan='3'>471</th>
+                            <td colspan='1'>7º</td><th colspan='3' width='200px'>UnFriendRedain & Darkest Starr</th><th colspan='3'>2</th><th colspan='3'>25</th><th colspan='3'>429</th>
+                        </tr>
+                        <tr>
+                            <td colspan='1'>8º</td><th colspan='3' width='200px'>joleno matador & Das_Trevas</th><th colspan='3'>2</th><th colspan='3'>23</th><th colspan='3'>388</th>
+                        </tr>
+                        <tr>
+                            <td colspan='1'>9º</td><th colspan='3' width='200px'>GTK & WEBRINK</th><th colspan='3'>1</th><th colspan='3'>40</th><th colspan='3'>649</th>
+                        </tr>
+                        <tr>
+                            <td colspan='1'>10º</td><th colspan='3' width='200px'>BABAYAGA & MESSI</th><th colspan='3'>1</th><th colspan='3'>29</th><th colspan='3'>471</th>
+                        </tr>
+                        <tr>
+                            <td colspan='1'>11º</td><th colspan='3' width='200px'>TOPG & Matapombo</th><th colspan='3'>1</th><th colspan='3'>15</th><th colspan='3'>278</th>
                         </tr>
                         
                         <tr>
-                            <td colspan='1'>8º</td><th colspan='3' width='200px'>GTK & WEBRINK</th><th colspan='3'>1</th><th colspan='3'>27</th><th colspan='3'>422</th>
+                            <td colspan='1'>12º</td><th colspan='3' width='200px'>Pantera Negra BR & -Maiomen_BR-</th><th colspan='3'>0</th><th colspan='3'>19</th><th colspan='3'>263</th>
                         </tr>
                         <tr>
-                            <td colspan='1'>9º</td><th colspan='3' width='200px'>TOPG & Matapombo</th><th colspan='3'>1</th><th colspan='3'>15</th><th colspan='3'>278</th>
+                            <td colspan='1'>13º</td><th colspan='3' width='200px'>GERARD_GT & Jesus Crhist</th><th colspan='3'>0</th><th colspan='3'>8</th><th colspan='3'>141</th>
+                        </tr>
+                        <tr>
+                            <td colspan='1'>14º</td><th colspan='3' width='200px'>SEXY & EMRAH</th><th colspan='3'>0</th><th colspan='3'>4</th><th colspan='3'>83</th>
                         </tr>
                         
-                        <tr>
-                            <td colspan='1'>10º</td><th colspan='3' width='200px'>Pantera Negra BR & -Maiomen_BR-</th><th colspan='3'>0</th><th colspan='3'>19</th><th colspan='3'>263</th>
-                        </tr>
-                        <tr>
-                            <td colspan='1'>11º</td><th colspan='3' width='200px'>GERARD_GT & Jesus Crhist</th><th colspan='3'>0</th><th colspan='3'>8</th><th colspan='3'>141</th>
-                        </tr>
-                        <tr>
-                            <td colspan='1'>12º</td><th colspan='3' width='200px'>SEXY & EMRAH</th><th colspan='3'>0</th><th colspan='3'>4</th><th colspan='3'>83</th>
-                        </tr>
+                        
         </table>
     </div>
     
