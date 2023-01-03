d4362620ce0b76cb93b148d1a88a240fa50b88ce
Adriano Martiliano <adrianomartiliano@gmail.com>
2023-01-03 17:41:17 -0300

    Adaptação do painel para o novo torneio, inacabado

--- painel.php
+++ painel.php
@@ -5,32 +5,30 @@
     
     $db = mysqli_select_db($conectar, 'u836711676_wwc');
 
-    $sql = mysqli_query($conectar, "SELECT * FROM inscritosx1 ORDER BY numerojogador") or die( 
-    mysqli_error($conectar) //caso haja um erro na consulta 
-    );
+    $sql = mysqli_query($conectar, "SELECT * FROM classificacaox2 ORDER BY pontos desc, kills desc, pontoscombate desc") or die( 
+        mysqli_error($conectar) //caso haja um erro na consulta 
+        );
 
-    $cont = 0;
-    $jogador = array();
-    while($aux = mysqli_fetch_assoc($sql)) {
-        $jogador[] = $aux["nickname"];
-    }
-    if(isset($_POST['submit'])){
-        $placaresq = $_POST['placaresq'];
-        $placardir = $_POST['placardir'];
-    
-        $pontosesq = $_POST['pontosesq'];
-        $pcesq = $_POST['pcesq'];
-    
-        $pontosdir = $_POST['pontosdir'];
-        $pcdir = $_POST['pcdir'];
-        
-        $jogadordir = $_POST['direita'];
-        $jogadoresq = $_POST['esquerdo'];
-    
-        $resultado1 = mysqli_query($conectar, "UPDATE inscritosx1 SET rodada2='$placaresq', pontos='$pontosesq', pontoscombate='$pcesq' WHERE nickname='$jogadoresq'");
-        $resultado2 = mysqli_query($conectar, "UPDATE inscritosx1 SET rodada2='$placardir', pontos='$pontosdir', pontoscombate='$pcdir' WHERE nickname='$jogadordir'");
-        echo '<script>alert("Sucesso")</script>';
-    }
+        $dupla = array();
+        while($aux = mysqli_fetch_assoc($sql)) {  
+            $dupla[] = $aux["dupla"];
+            $pontos[] = $aux["pontos"];
+            $kills[] = $aux["kills"];
+            $pc[] = $aux["pontoscombate"];
+            $contato[] = $aux["contato"];
+        }
+        if(isset($_POST['submit'])){
+
+            $duplaesq = $_POST['duplaesq'];
+            $dupladir = $_POST['dupladir'];
+            $kesq = $_POST['kesq'];
+            $kdir = $_POST['kdir'];
+            $pcesq = $_POST['pcesq'];
+            $pcdir = $_POST['pcdir'];
+
+            $alteracao1 = mysqli_query($conectar, "UPDATE classificacaox2 SET kills = '$kesq', pontoscombate='$pcesq' WHERE dupla='$duplaesq'");
+            $alteracao1 = mysqli_query($conectar, "UPDATE classificacaox2 SET kills = '$kdir', pontoscombate='$pcdir' WHERE dupla='$dupladir'");
+        }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,7 +37,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/painel.css">
+    <script src="https://kit.fontawesome.com/35d9b76d78.js" crossorigin="anonymous"></script>
     <title>ADM</title>
+    <style>
+        td, th{
+            padding: 5px;
+        }
+        table{
+            margin: 40px auto;
+        }
+        select{
+            width: 180px;
+            margin: 10px;
+        }
+        
+    </style>
 </head>
 <body>
     <h1>Bem vindo, <?php echo $_SESSION['usuario']; ?></h1>
@@ -48,22 +60,24 @@
     <div class="inserirRodada">
         <form action="painel.php" method="post">
             <?php
-                echo "<div class='linha1'><h1>RODADA 3</h1><select name='esquerdo' id='esquerdo'>";
-                for ($i=0; $i<32; $i++){
-                    echo "<option value='". $jogador[$i]; echo "'>". $jogador[$i]; echo "</option>";
+                echo "<div id='linha1select'><div><select name='duplaesq'>";
+                for ($i=0; $i<count($dupla); $i++){
+                    echo "<option value='". $dupla[$i]; echo "'>". $dupla[$i]; echo "</option>";
                 }
-                echo "</select> X ";
-                echo "<select name='direita'>";
-                for ($i=0; $i<32; $i++){
-                    echo "<option value='". $jogador[$i]; echo "'>". $jogador[$i]; echo "</option>";
+                echo "</select>X";
+                echo "<select name='dupladir'>";
+                for ($i=0; $i<count($dupla); $i++){
+                    echo "<option value='". $dupla[$i]; echo "'>". $dupla[$i]; echo "</option>";
                 }
-                echo "</select><br><br>";
+                echo "</select></div>";
 
-                echo "<input type='number' name='placaresq' id='placaresq'> X <input type='number' name='placardir' id='placardir'><br><br>";
-                
-                echo "<input type='number' name='pontosesq' placeholder='P'><input type='number' name='pcesq' placeholder='PC'> X <input type='number' name='pontosdir' placeholder='P'>";
-                echo "<input type='number' name='pcdir' placeholder='PC'><br>";
-                echo "<input type='submit' value='SALVAR' name='submit' id='btn-salvar'></div>";
+                echo "
+                    <div><input type='number' name='kesq' placeholder='Kills'> X
+                    <input type='number' name='kdir' placeholder='Kills'></div>
+                    <input type='number' name='pcesq' placeholder='PC'> X 
+                    ";
+                echo "<input type='number' name='pcdir' placeholder='PC'>";
+                echo "<div><input type='submit' value='SALVAR' name='submit' id='btn-salvar'></div></div>";
             ?>
         </form>
     </div>
