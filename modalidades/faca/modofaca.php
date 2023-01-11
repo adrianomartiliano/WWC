b71a852b7663b503b1ada421cbabd77b77b5cfb0
Adriano Martiliano <adrianomartiliano@gmail.com>
2023-01-11 12:38:32 -0300

    Página de inscricão torneio modo faca

--- modalidades/faca/modofaca.php
+++ modalidades/faca/modofaca.php
@@ -9,12 +9,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/menu.css">
+    <link rel="stylesheet" href="/wwc/css/x1.css">
     <title>Modo Faca</title>
 </head>
 <body>
 
+<?php
+    require_once 'formulario.html';
+?>
+
     
 </body>
 </html>
-<script src="../../js/menu.js"></script>
