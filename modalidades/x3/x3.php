216095a7fe5c6c8f35d07bb29f3885c48d71445a
Adriano Martiliano <adrianomartiliano@gmail.com>
2023-01-31 11:54:11 -0300

    Início da estrutura da lista de inscritos.

--- modalidades/x3/x3.php
+++ modalidades/x3/x3.php
@@ -4,16 +4,17 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>X3</title>
 </head>
 <body>
 
     <?php
+        require_once 'inscritos.php';
         require_once 'formulario.html';
     ?>
     
