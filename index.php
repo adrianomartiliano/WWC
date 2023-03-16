7bad52722c72a17bc3f20807829be6459d7d18db
Adriano Martiliano <adrianomartiliano@gmail.com>
2023-03-16 13:40:04 -0300

    Remanejamento de arquivos
    Criação da pasta OLD para guardar os dados antigos.

--- index.php
+++ index.php
@@ -1,55 +1,24 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     
-    <title>WWC</title>
-    <style>
-        
-        .cardindex{
-            width: 384px;
-            height: 226px;
-            border: 1px solid gray;
-            border-radius: 5px;
-            margin: 20px auto;
-        }
-        #x3menu{
-            background-image: url("images/x3menu.png");
-            background-position: center;
-            background-size: cover;
-        }
-        #x2menu{
-            background-image: url("images/x2menu.png");
-            background-position: center;
-            background-size: cover;
-        }
-        #x1faca{
-            background-image: url("images/x1faca.png");
-            background-position: center;
-            background-size: cover;
-        }
-        #campeoesmenu{
-            background-image: url("images/campeoesmenu.png");
-            background-position: center;
-            background-size: cover;
-        }
-
-    </style>
+    <title>Document</title>
 </head>
+<style>
+    *{
+        margin: 0px;
+    }
+    h1{
+        text-align: center;
+    }
+</style>
 <body>
+    <h1>FIM</h1>
     
-    <?php
-        require_once 'menu.html';
-    ?>
-    <div class="center">
-        <a href="modalidades/x3/"><div class="cardindex" id="x3menu"></div></a>
-        <a href="x2.php"><div class="cardindex" id="x2menu"></div></a>
-        <a href="modalidades/faca/"><div class="cardindex" id="x1faca"></div></a>
-        <a href="campeoes.php"><div class="campeoes cardindex" id="campeoesmenu"><h1></h1></div></a>
-    </div>
-    
-</body>
 
+    <div class="tenor-gif-embed" data-postid="9466062" data-share-method="host" data-aspect-ratio="1.85185" data-width="100%"><a href="https://tenor.com/view/goku-flying-dragon-ball-z-goodbye-the-end-gif-9466062">Goku Flying GIF</a>from <a href="https://tenor.com/search/goku-gifs">Goku GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+        <h1>THE END</h1>
+</body>
 </html>
