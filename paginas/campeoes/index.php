c3f4532818bfd4ec3fa5f2ee013fc9c3d5da012d
Adriano Martiliano <adrianomartiliano@gmail.com>
2023-05-09 17:41:48 -0300

    Fundo animado

--- paginas/campeoes/index.php
+++ paginas/campeoes/index.php
@@ -9,133 +9,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campeões</title>
+    <style>
+        /* ---- reset ---- */ 
+        body{ 
+            margin:0; 
+            font:normal 75% Arial, Helvetica, sans-serif; 
+        } 
+        canvas{ 
+            display: block; 
+            vertical-align: bottom; 
+        } /* ---- particles.js container ---- */ 
+        #particles-js{ position:absolute; width: 100%; height: 100%; background-color: #ffab07; margin-top:-60px; } 
+        /* ---- stats.js ---- */ 
+        .count-particles{ background: #000022; position: absolute; top: 48px; left: 0; width: 80px; color: #13E8E9; font-size: .8em; text-align: left; text-indent: 4px; line-height: 14px; padding-bottom: 2px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; } 
+        .js-count-particles{ font-size: 1.1em; } 
+        #stats, .count-particles{ -webkit-user-select: none; margin-top: 5px; margin-left: 5px; } 
+        #stats{ border-radius: 3px 3px 0 0; overflow: hidden; } 
+        .count-particles{ border-radius: 0 0 3px 3px; }
+        </style>
 </head>
 <body>
 <section class="campeoes">
-        <div class="center">
-            <div class="menucampeoes"><h1 id="menuclans" onClick="clanvisivel()">CLANS</h1><h1 id="menujogadores" onClick="jogadoresvisivel()" style="color:gray">JOGADORES</h1></div>
-            <div class="conteudo-campeoes">
-                <!-- <div id="clans-campeoes">
-                    <div class="list-clan">
-                        <p>TWIN</p>
-                        <div class="trofeus">
-                            <img src="../../images/ouro.png" alt="">
-                            <img src="../../images/ouro.png" alt="">
-                            <img src="../../images/prata.png" alt="">
-                            <img src="../../images/bronze.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-clan">
-                        <p>WU</p>
-                        <div class="trofeus">
-                            <img src="../../images/ouro.png" alt="">
-                            <img src="../images/bronze.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-clan">
-                        <p>CAN</p>
-                        <div class="trofeus">
-                            <img src="../images/prata.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-clan">
-                        <p>MUND</p>
-                        <div class="trofeus">
-                            <img src="../images/prata.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-clan">
-                        <p>_GE_</p>
-                        <div class="trofeus">
-                            <img src="../images/bronze.png" alt="">
-                        </div>
-                    </div>
-                    
-                </div>
-                <div id="jogadores-campeoes" class="inativo">
-                    <div class="list-jogadores">
-                        <p>Joey BRICKWALL</p>
-                        <div class="trofeus">
-                            <img src="../images/ouro.png" alt="">
-                            <img src="../images/bronze.png" alt="">    
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>KING PITBULL</p>
-                        <div class="trofeus">
-                            <img src="../images/ouro.png" alt="">
-                            <img src="../images/bronze.png" alt="">    
-                        </div>        
-                    </div>
-                    <div class="list-jogadores">
-                        <p>B O S S</p>
-                        <div class="trofeus">
-                            <img src="../images/ouro.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>1420lopez</p>
-                        <div class="trofeus">
-                            <img src="../images/ouro.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>ILLUSTRIOUS ONER</p>
-                        <div class="trofeus">
-                            <img src="../images/ouro.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>G Ф K Ц</p>
-                        <div class="trofeus">
-                            <img src="../images/prata.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>LeLecф_Ф_Mдu_BЯ</p>
-                        <div class="trofeus">
-                            <img src="../images/prata.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>Maltryx</p>
-                        <div class="trofeus">
-                            <img src="../images/prata.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>BLДCK -Ф- KELLДЯ</p>
-                        <div class="trofeus">
-                            <img src="../images/prata.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>M E S S I</p>
-                        <div class="trofeus">
-                            <img src="../images/prata.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>GOD OF WAR</p>
-                        <div class="trofeus">
-                            <img src="../images/bronze.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>BLESSED</p>
-                        <div class="trofeus">
-                            <img src="../images/bronze.png" alt="">
-                        </div>
-                    </div>
-                    <div class="list-jogadores">
-                        <p>TERMINATOR</p>
-                        <div class="trofeus">
-                            <img src="../images/bronze.png" alt="">
-                        </div>
-                    </div>
-                    
-                </div> -->
-            </div>
+        <!-- particles.js container --> 
+        <div id="particles-js"></div> 
+        <!-- stats - count particles --> 
+        <div class="count-particles">  
+            <!-- particles.js lib - https://github.com/VincentGarreau/particles.js --> 
+            <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js">
+            </script> <!-- stats.js lib --> 
+            <script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
         </div>
     </section>
 
@@ -158,6 +60,8 @@
         textjogadores.style.color = "black";
         textclan.style.color = "gray";
     }
+    
+    particlesJS("particles-js", {"particles":{"number":{"value":178,"density":{"enable":true,"value_area":552.4033491425909}},"color":{"value":"#e0453d"},"shape":{"type":"star","stroke":{"width":0,"color":"#41370b"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":8.017060304327606,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"window","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
 </script>
 </html>
 
