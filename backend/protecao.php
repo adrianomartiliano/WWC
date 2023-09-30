8afdddb1c1e72d33a55e56b77774fc6f72c95c85
Adriano Martiliano <adrianomartiliano@gmail.com>
2023-09-30 20:50:38 -0300

    Sistema de Login
    - Sistema de login
    - Direcionamento para um painel
    - Proteção a páginas privadas somente para usuários logados

--- backend/protecao.php
+++ backend/protecao.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION)){
     session_start();
 }
 
-if(!isset($_SESSION['usuario'])){
+if(!isset($_SESSION['NICKNAME'])){
     echo '<script>window.history.go(-1);</script>';
 }
 
--- paginas/login/painel.php
+++ paginas/login/painel.php
@@ -8,11 +8,26 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="painel.css">
         <title>Dash</title>
     </head>
     <body>
+        <div class="barraStatusLogin">
+            <p><?php
+                echo "Bem vindo, " . $_SESSION['NICKNAME'];
+            ?></p>
 
-        <h2>Em breve os torneios aqui!</h2>
+            <p>
+                <a href="../../backend/logout.php">Sair</a>
+            </p>
+        </div>
+
+        <div class="painelArea">
+            <div class="torneioX1">
+                <p>Em breve inscrição do torneio aqui</p>
+            </div>
+
+        </div>
         
     </body>
     </html>
